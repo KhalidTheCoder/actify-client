68bde16014c5f68cf61ffd04a90fa35eba757079
Khalidahsan007 <khalidasan2407@gmail.com>
2025-07-13 20:29:13 +0600

    event data saved to db functinality added

--- src/pages/CreateEvent.jsx
+++ src/pages/CreateEvent.jsx
@@ -1,7 +1,13 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { forwardRef } from "react";
+import { format } from "date-fns";
+import toast from "react-hot-toast";
+import axios from "axios";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router";
+import { AuthContext } from "../context/AuthContext";
 
 const CustomInput = forwardRef(({ value, onClick }, ref) => (
   <button
@@ -15,14 +21,49 @@ const CustomInput = forwardRef(({ value, onClick }, ref) => (
 ));
 
 const CreateEvent = () => {
+  const navigate = useNavigate();
   const [eventDate, setEventDate] = useState(null);
+  const { user } = useContext(AuthContext);
+
+  const handleAddEvent = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries());
+
+    if (eventDate) {
+      data.date = format(eventDate, "yyyy-MM-dd");
+      data.email = user?.email
+    } else {
+      return toast.error("Please select an event date.");
+    }
+
+    console.log(data);
+    axios
+      .post("http://localhost:3000/events", data)
+      .then((res) => {
+        if (res.data.insertedId) {
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Event Has Been Created!!",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          navigate("/upcoming-events");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <div className="max-w-7xl mx-auto">
       <h1>Create Event</h1>
 
-      <form>
-        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+      <form onSubmit={handleAddEvent}>
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs max-w-6xl border p-4">
           <legend className="fieldset-legend">Event Details</legend>
 
           <label className="label">Title</label>
@@ -41,7 +82,11 @@ const CreateEvent = () => {
           ></textarea>
 
           <label className="label">Event Type</label>
-          <select defaultValue="Pick a color" className="select">
+          <select
+            defaultValue="Pick a color"
+            name="event-type"
+            className="select"
+          >
             <option disabled={true}>Select Event Type</option>
             <option>Cleanup</option>
             <option>Plantation</option>
@@ -65,20 +110,20 @@ const CreateEvent = () => {
           />
 
           <label className="label">date</label>
-          
-          <label className="label">Event Date</label>
-            <DatePicker
-              selected={eventDate}
-              onChange={(date) => setEventDate(date)}
-              minDate={new Date()}
-              customInput={<CustomInput />}
-              required
-            />
 
-            <button type="submit" className="btn w-full mt-3">Create Event</button>
-          
+          <label className="label">Event Date</label>
+          <DatePicker
+            selected={eventDate}
+            onChange={(date) => setEventDate(date)}
+            minDate={new Date()}
+            customInput={<CustomInput />}
+            required
+          />
+
+          <button type="submit" className="btn w-full mt-3">
+            Create Event
+          </button>
         </fieldset>
-        
       </form>
     </div>
   );
