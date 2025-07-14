f344b15b493c1e566c7a09ec6c94eb738cdb8db2
Khalidahsan007 <khalidasan2407@gmail.com>
2025-07-14 08:06:04 +0600

    location icon from react added also fixed font of join and view btn

--- src/components/UpComingEventsCard.jsx
+++ src/components/UpComingEventsCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router";
+import { FaMapLocation } from "react-icons/fa6";
 
 const UpComingEventsCard = ({ event }) => {
   const {
@@ -19,12 +20,13 @@ const UpComingEventsCard = ({ event }) => {
       />
      <div className="px-10 py-10">
          <h2 className="text-xl font-semibold text-[#493628]">{title}</h2>
-      <p className="text-sm font-medium text-[#AB886D] mt-1">📍 {location}</p>
+      <p className="text-sm font-medium text-[#AB886D] flex items-center gap-1 mt-1"> <span className="text-[#493628]">
+        <FaMapLocation /></span> {location}</p>
       <p className="text-sm font-medium text-[#AB886D] italic">{eventType}</p>
       <p className="text-sm font-medium text-[#493628] mt-1">📅 {date}</p>
 
       <Link to={`/event/${event.eventId || event._id}`}>
-        <button className="mt-4 w-full py-2 rounded bg-[#AB886D] text-white hover:bg-[#493628] transition duration-300">View Event</button>
+        <button className="mt-4 w-full py-2 rounded bg-[#AB886D] font-medium text-white hover:bg-[#493628] transition duration-300">View Event</button>
       </Link>
      </div>
     </div>
